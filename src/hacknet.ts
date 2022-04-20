56db21f1a2aca02ad5219740147f29393f5bfb38
abyo <alexis.ollandfr@gmail.com>
2022-04-20 15:28:31 +0200

    Modify hacknet script to include node median rates

--- src/hacknet.ts
+++ src/hacknet.ts
@@ -1,12 +1,5 @@
 import { NS } from "@ns";
 
-/**
- * Given a start, end, and step, return an array of numbers from start to end, incrementing by step.
- * @param {number} start - The first number in the sequence.
- * @param {number} end - The end of the range.
- * @param {number} [step=1] - The step value. Default is 1.
- * @returns An array of numbers from start to end, with a step of 1.
- */
 function range(start: number, end: number, step = 1): number[] {
   const arr = [];
   for (let i = start; i < end; i += step) {
@@ -18,25 +11,53 @@ function range(start: number, end: number, step = 1): number[] {
 function calculateHnetMoneyRate(
   level: number,
   ram: number,
-  cores: number
+  cores: number,
+  mult: number
 ): number {
   const levelMult = level * 1.5;
   const ramMult = Math.pow(1.035, ram - 1);
   const coresMult = (cores + 5) / 6;
-  return levelMult * ramMult * coresMult;
+  return levelMult * ramMult * coresMult * mult;
+}
+
+function calculateMedianNodeRate(ns: NS, nodes: number[]): number {
+  const totalLevel: number[] = [];
+  const totalRam: number[] = [];
+  const totalCore: number[] = [];
+  const nodesNumber: number = ns.hacknet.numNodes();
+
+  for (const node of nodes) {
+    const { level, ram, cores } = ns.hacknet.getNodeStats(node);
+    totalLevel.push(level);
+    totalRam.push(ram);
+    totalCore.push(cores);
+  }
+
+  const medianLevel = totalLevel.reduce((a, b) => a + b, 0) / nodesNumber;
+  const medianRam = totalRam.reduce((a, b) => a + b, 0) / nodesNumber;
+  const medianCore = totalCore.reduce((a, b) => a + b, 0) / nodesNumber;
+
+  const nodeMedianRate = calculateHnetMoneyRate(
+    medianLevel,
+    medianRam,
+    medianCore,
+    ns.getPlayer().hacknet_node_money_mult
+  );
+
+  return nodeMedianRate;
 }
 
 export async function main(ns: NS): Promise<void> {
   ns.disableLog("sleep");
 
   while (true) {
-    const playerMoney = ns.getPlayer().money;
     const playerMult = ns.getPlayer().hacknet_node_money_mult;
+    const playerMoney = ns.getPlayer().money;
     const nodes = range(0, ns.hacknet.numNodes());
     const nodeStats = [];
 
     const nodePurchaseCost = ns.hacknet.getPurchaseNodeCost();
-    const nodePurchaseRate = calculateHnetMoneyRate(100, 3, 7) * playerMult;
+    const nodePurchaseRate = calculateMedianNodeRate(ns, nodes);
 
     nodeStats.push({
       name: "node",
@@ -52,11 +73,11 @@ export async function main(ns: NS): Promise<void> {
       const coreUpgradeCost = ns.hacknet.getCoreUpgradeCost(node, 1);
 
       const levelUpgradeRate =
-        calculateHnetMoneyRate(level + 1, ram, cores) * playerMult - production;
+        calculateHnetMoneyRate(level + 1, ram, cores, playerMult) - production;
       const ramUpgradeRate =
-        calculateHnetMoneyRate(level, ram + 1, cores) * playerMult - production;
+        calculateHnetMoneyRate(level, ram + 1, cores, playerMult) - production;
       const coreUpgradeRate =
-        calculateHnetMoneyRate(level, ram, cores + 1) * playerMult - production;
+        calculateHnetMoneyRate(level, ram, cores + 1, playerMult) - production;
 
       nodeStats.push(
         {
@@ -83,16 +104,15 @@ export async function main(ns: NS): Promise<void> {
     nodeStats.sort((a, b) => b.ratio - a.ratio);
     const upgrade = nodeStats[0];
 
-    ns.tprint(upgrade);
-
-    if (upgrade.cost > playerMoney) ns.print("Not enough money to upgrade!");
+    if (upgrade.cost > playerMoney)
+      ns.print("Not enough money for the upgrade (hacknet)!");
 
     if (upgrade.name === "level") {
-      ns.hacknet.upgradeLevel(upgrade.core, 1);
+      ns.hacknet.upgradeLevel(upgrade.core as number, 1);
     } else if (upgrade.name === "ram") {
-      ns.hacknet.upgradeRam(upgrade.core, 1);
+      ns.hacknet.upgradeRam(upgrade.core as number, 1);
     } else if (upgrade.name === "core") {
-      ns.hacknet.upgradeCore(upgrade.core, 1);
+      ns.hacknet.upgradeCore(upgrade.core as number, 1);
     } else if (upgrade.name === "node") {
       ns.hacknet.purchaseNode();
     } else {
