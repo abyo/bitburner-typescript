ee267cecceb86a16f30409784ed3167429f89ae5
abyo <alexis.ollandfr@gmail.com>
2022-05-03 18:35:43 +0200

    Add backdoor terminal command to find_server

--- src/scripts/findServer.ts
+++ src/scripts/findServer.ts
@@ -2,5 +2,5 @@ import { NS } from '@ns';
 import { findServer } from '/compiler/utilities';
 
 export function main(ns : NS) : void {
-  ns.tprint(`\nhome; connect ${findServer(ns, ns.getHostname(), <string>ns.args[0], []).slice(1).join('; connect ')}`);
+  ns.tprint(`\nhome; connect ${findServer(ns, ns.getHostname(), <string>ns.args[0], []).slice(1).join('; connect ')}; backdoor;`);
 }
